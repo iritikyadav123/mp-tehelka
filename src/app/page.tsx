b000b565751674c27f0f66b60595883f0bc157a5
iritikyadav123 <iritikyadav123@gmail.com>
2024-08-13 02:07:50 +0530

    ff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,13 +17,13 @@ export default async function Home() {
             </div>
             <>
                 <div className="snap-x mx-auto snap-mandatory h-[100rem] flex w-auto overflow-x-scroll justify-evenly gap-4 sm:gap-2 lg:ml-2 no-scrollbar mt-10">
-                    <div className="snap-start bg-pink-300 w-[23rem] flex-col gap-4 flex-shrink-0 h-[30rem] flex items-center justify-center text-8xl rounded-md overflow-auto no-scrollbar">
+                    <div className="snap-start  w-[23rem] flex-col gap-4 flex-shrink-0 h-[30rem] flex items-center justify-center text-8xl rounded-md overflow-auto no-scrollbar">
                         <ArticleCard className="h-22 w-[90%]" name="ritikyadav" position="unEmployed" title={"lorem kesa rista hai yei kese sapne hai vegane hoklar kyu lqgte apne hai mai soch ami"} />
                     </div>
                     <div className="snap-center h-full   w-[23rem] sm:w-[35rem] flex-shrink-0  flex items-center justify-center text-8xl rounded-md">
                             <TrandingNews />
                     </div>
-                    <div className="bg-green-500 snap-end  w-[23rem]  flex-shrink-0 h-[30rem] flex items-center justify-center text-8xl rounded-md">
+                    <div className=" snap-end  w-[23rem]  flex-shrink-0 h-[30rem] flex items-center justify-center text-8xl rounded-md">
                         <AllNews />
                     </div>
                 </div>
